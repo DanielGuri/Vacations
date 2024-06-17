c4385582e0faec4dc153d443a6115c7995bac49b
Daniel Gurevich <166417010+DanielGuri@users.noreply.github.com>
2024-06-17 13:06:21 +0300

    Delete Vacation project directory

--- Vacation project/backend/helper/loggedUser.js
+++
@@ -1,7 +0,0 @@
-module.exports.loggedUser = (req, res, next) => {
-    if (req.session.username) {
-        next()
-    }else {
-        res.status(401).send({err: "Its only for logged users"})
-    }
-}
